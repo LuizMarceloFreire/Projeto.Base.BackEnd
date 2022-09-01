30e17b0b8354dea9c1e920a845361a8c65e5f632
Luiz Marcelo Freire <luiz.marcelo-developer@outlook.com>
2022-09-01 16:06:52 -0300

    Implementado médoto para alterar um clube

---
+++ src/Services/Projeto.Base.BackEnd.Services/Handlers/Clube/EditarClubeHandler.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using Projeto.Base.BackEnd.Application.Commands.Clube;
+using Projeto.Base.BackEnd.Domain.Entidades.Clubes.Interfaces;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Projeto.Base.BackEnd.Services.Handlers.Clube
+{
+    public class EditarClubeHandler : AsyncRequestHandler<EditarClubeCommand>
+    {
+        private readonly IClubeRepositorio _repositorio;
+
+        public EditarClubeHandler(IClubeRepositorio repositorio)
+        {
+            _repositorio = repositorio;
+        }
+
+        protected override async Task Handle(EditarClubeCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var clube = await _repositorio.ObterPorIdAsync(request.ClubeId);
+
+                if (clube == null)
+                    throw new Exception("Nenhum clube encontrado.");
+
+                clube.AlterarNome(request.Nome);
+                clube.AlterarAnoFundacao(request.AnoFundacao);
+                clube.AlterarUrlRedeSocial(request.UrlRedeSocial);
+                clube.AlterarDataAlteracao(request.DataAlteracao);
+                clube.AlterarSituacao(request.Ativo);
+
+                _repositorio.Atualizar(clube);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
